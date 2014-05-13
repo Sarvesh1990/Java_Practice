dbc75cc2d7be7adb76474bb85f39e4b0066215be
Sarvesh1990 <sarveshiitb@gmail.com>
2014-05-13 17:38:46 +0530

    Added actual print again

--- Topo_Search/src/main.java
+++ Topo_Search/src/main.java
@@ -40,8 +40,7 @@ public class main
 		Graph myInvGraph = myGraph.Invert();
 		for (Integer key : myInvGraph.myMap.keySet())
 		{
-			//System.out.println("Git testing");
-			//System.out.println("For key "+ key + " set is " + myInvGraph.myMap.get(key));
+			System.out.println("For key "+ key + " set is " + myInvGraph.myMap.get(key));
 		}
 	}
 }
