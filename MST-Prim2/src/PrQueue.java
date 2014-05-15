b5a6c9c45f2d81e15ec607aa9c1176a07f93eb2b
Sarvesh1990 <sarveshiitb@gmail.com>
2014-05-16 01:50:53 +0530

    MST-Prim2 working

--- MST-Prim2/src/PrQueue.java
+++ MST-Prim2/src/PrQueue.java
@@ -1,39 +1,53 @@
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Set;
-import java.util.TreeMap;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class PrQueue 
 {
-	Map<Integer, Edge> myPrMap = new TreeMap <Integer, Edge> ();
+	PriorityQueue<Edge> myQueue = new PriorityQueue<Edge>();
+	Edge[] myArray = {null, null, null, null, null, null, null, null};
+	boolean[] myMarkArray = {false, false, false, false, false, false, false, false};
+	
 	void CreateMST(MSTree myMSTree, int index, Map<Integer, Set<Edge> > myMap)
 	{
-		//while(myMSTree.mySet.size()!=7)
-		//{
+		myMarkArray[index]=true;
+		while(myMSTree.mySet.size()!=7)
+		{
 			for(Edge myEdge : myMap.get(index))
 			{
-				if(myPrMap.get(myEdge.Other(index))!=null)
+				if(myMarkArray[myEdge.Other(index)])
+					continue;
+				if(myArray[myEdge.Other(index)] == null)
 				{
-					if(myEdge.weight < myPrMap.get(myEdge.Other(index)).weight)
-					{
-						myPrMap.put(myEdge.Other(index), myEdge);
-					}
+					System.out.println("Array null, adding "+myEdge.Other(index));
+					myArray[myEdge.Other(index)]=myEdge;
+					myQueue.add(myEdge);
 				}
 				else
 				{
-					myPrMap.put(myEdge.Other(index), myEdge);
+					if(myArray[myEdge.Other(index)].weight>myEdge.weight)
+					{
+						//System.out.println("Size smaller, updating "+myEdge.Other(index));
+						myArray[myEdge.Other(index)]=myEdge;
+						myQueue.remove(myArray[myEdge.Other(index)]);
+						myQueue.add(myEdge);
+					}
 				}
 			}
-			for (Map.Entry<Integer, Edge> entry : myPrMap.entrySet())
-			{
-				System.out.println(entry.getKey() + "/" + entry.getValue());
-			}
-			
+			Edge myEdge = myQueue.poll();
+			while(myMarkArray[myEdge.Either()] && myMarkArray[myEdge.Other(myEdge.Either())])
+				myEdge=myQueue.poll();
+			if(myMarkArray[myEdge.Either()])
+				index = myEdge.Other(myEdge.Either());
+			else
+				index = myEdge.Either();
+			//System.out.println("Indexex are " + myEdge.Either()+" "+myEdge.Other(myEdge.Either())+" with index set as "+index);
+			myMSTree.mySet.add(myEdge);
+			myMarkArray[index]=true;
 		}
-	//}
+	}	
 }
